07ba48295b99ecd3afc0cfebda13909422e1051f
agung-margi <agung.margyono@gmail.com>
2025-08-15 14:06:33 +0700

    feat: update roles be

--- src/modules/transaksi/transaksi.route.ts
+++ src/modules/transaksi/transaksi.route.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express'
 import { approvePermintaan, confirmTAG, createPenerimaan, createPermintaan, createTAG, createTransaksiOut, getAllPenerimaan, getAllPengeluaran, getAllPermintaan, getAllTAG } from './transaksi.controller'
-import { requireAdmin, requireAuth, requireManager, requireStaffSO } from '../../middlewares/auth.middleware'
+import { requireAdmin, requireAuth, requireManager, requireStaffSO, requireUser } from '../../middlewares/auth.middleware'
 
 
 export const TransaksiRouter: Router = Router()
-TransaksiRouter.post('/pengeluaran', requireAuth, createTransaksiOut)
+TransaksiRouter.post('/pengeluaran', requireAuth, requireStaffSO,createTransaksiOut)
 TransaksiRouter.get('/pengeluaran', requireAuth, getAllPengeluaran)
 TransaksiRouter.post('/penerimaan', requireAuth, requireStaffSO, createPenerimaan)
 TransaksiRouter.get('/penerimaan', requireAuth, getAllPenerimaan)
-TransaksiRouter.post('/permintaan', requireAuth, createPermintaan)
+TransaksiRouter.post('/permintaan', requireAuth, requireUser, createPermintaan)
 TransaksiRouter.put('/permintaan/:id/approve', requireAuth, requireManager, approvePermintaan)
 TransaksiRouter.get('/permintaan/:id', requireAuth, getAllPermintaan)
 TransaksiRouter.get('/permintaan', requireAuth, getAllPermintaan)
-TransaksiRouter.post('/tag', requireAuth, requireAdmin, createTAG)
+TransaksiRouter.post('/tag', requireAuth, requireStaffSO, createTAG)
 TransaksiRouter.post('/tag/confirm', requireAuth, requireAdmin, confirmTAG)
 TransaksiRouter.get('/tag', requireAuth, requireAdmin, getAllTAG)
